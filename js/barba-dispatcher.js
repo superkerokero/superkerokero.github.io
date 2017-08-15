c0606cbeb8d15b7d4f084198da76efcde333516c
superkerokeroMacOS <superkerokero@outlook.com>
2017-08-15 20:39:15 +0900

    Restored particle effects.

--- js/barba-dispatcher.js
+++ js/barba-dispatcher.js
@@ -2,10 +2,12 @@ Barba.Dispatcher.on('newPageReady', function(currentStatus, oldStatus, container
     switch (currentStatus.namespace) {
         case "index":
             $(".site-heading").html("<h1>Blog</h1><span class='subheading'>Good Good Study  Day Day Up</span>");
+            particlesJS.load('particles-js', 'assets/particles-grid.json');
             console.log('index');
             break;
         case "about":
             $(".site-heading").html("<h1>About</h1><span class='subheading'>It takes one to know one</span>");
+            particlesJS.load('particles-js', 'assets/particles-star.json');
             //index.onEnter();
             console.log('about');
             break;
@@ -14,16 +16,19 @@ Barba.Dispatcher.on('newPageReady', function(currentStatus, oldStatus, container
             var postItem = localStorage.getItem("postSrc");
             $("#myIframe").attr("src", postItem);
             $("#myIframe").attr("width", $(".col-lg-8.offset-lg-2.col-md-10.offset-md-1").width());
+            particlesJS.load('particles-js', 'assets/particles-star.json');
             //index.onEnter();
             console.log('about');
             break;
         case "playground":
             $(".site-heading").html("<h1>Playground</h1><span class='subheading'>A thing of beauty is a joy forever</span>");
+            particlesJS.load('particles-js', 'assets/particles-bubble.json');
             //index.onEnter();
             console.log('about');
             break;
         case "contact":
             $(".site-heading").html("<h1>Contact</h1><span class='subheading'>No man is an island.</span>");
+            particlesJS.load('particles-js', 'assets/particles-snow.json');
             //index.onEnter();
             console.log('about');
             break;
